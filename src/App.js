6c69d342c141c48bc0c7c805ce5e60d72c2b1032
majeedaisha <majeedaisha44@gmail.com>
2022-03-24 07:46:55 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ProfileForm from './components/ProfileForm'
+import ProfileCard from './components/ProfileCard'
 
 function App() {
+  const[allProfiles, setALLProfiles]=useState([
+{
+firstname:"Majeed",
+lastname:"Aisha",
+email:"majeedaisha44@gmail.com",
+phone:"0594623170"
+}
+])
+const updateProfiles =(profile)=>{
+let arr =allProfiles;
+arr.push(profile);
+setALLProfiles([...arr]);
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+    <h1>Profile Maker</h1>
+    <div>
+    <ProfileForm submit={updateProfiles}/>
+    <hr/>
+    <div className='list'>
+    {allProfiles.map((person,index)=>(
+<ProfileCard key={index} card={person}/>
+    ))}
+    
     </div>
-  );
+    </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ProfileForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+function ProfileForm({submit}) {
+const [profile, setProfile]=useState({
+firstname:"",
+lastname:"",
+phone:"",
+    });
+
+const handler =(event) =>{
+setProfile((prev)=>({
+    ...prev,
+    [event.target.name]: event.target.value
+}));
+}
+
+const handleForm =(e)=>{
+    e.preventDefault();
+    submit(profile)
+};
+
+  return (
+<div className='formContainer'>
+<h3>Profile Form</h3>
+<form>
+<fieldset>
+<legend>Bio Data</legend> 
+<div className='names'>
+
+
+ <label> 
+First name
+<input name='firstname' type="text" onChange={handler}/>
+</label>
+
+ <label> 
+Last name 
+<input name='lastname' type="text" onChange={handler}/>
+</label>
+
+ <label> 
+Email
+<input name='email' type="email" onChange={handler}/>
+</label>
+
+ <label> 
+Phone
+<input name='phone' type="tel" onChange={handler}/>
+</label> 
+</div>
+</fieldset>
+ <button className='form' onClick={handleForm}>Add Profile</button>   
+</form>
+    
+</div>
+  )
+}
+
+export default ProfileForm
